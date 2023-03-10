5a7d2ff9ca8c19bc80dfe00acad5579117184331
stepkos <stepkos@wp.pl>
2023-03-10 14:10:56 +0100

    Some clear

--- 2. Iteratory - filtered with predicate/Iteratory/src/main/java/org/example/Main.java
+++ 2. Iteratory - filtered with predicate/Iteratory/src/main/java/org/example/Main.java
@@ -1,16 +1,8 @@
 package org.example;
 
-import java.util.Scanner;
-
 public class Main {
 
     private static void demo() {
-//        Scanner scanner = new Scanner(System.in);
-//        String baseFilePath = "input/";
-//        System.out.println("Prosze podac nazwe pliku w folderze 'input', ktory ma zostac wczytany");
-//        String fileName = scanner.nextLine();
-//        scanner.close();
-//        StudentList studentList = StudentList.fromFile(baseFilePath + fileName);
 
         StudentList studentList = new StudentList(new Student[]{
                 new Student(123456, "Jan", "Nowak", 4.5),
@@ -28,14 +20,6 @@ public class Main {
         System.out.println("\nZmieniamy srednia ocen studenta o indeksie 654321 na 5.0 i wyswietlamy cala liste wszytskich studentow");
         studentList.changeStudentGrateAvg(654321, 5);
         studentList.printAllStudents();
-
-//        System.out.println("\nTworzymy nowa liste studentow z ocenemi ponizej 3.0 i wyswietlamy ja");
-//        StudentList studentsBelow3 = studentList.getStudentsBelow3();
-//        studentsBelow3.printAllStudents();
-
-//        System.out.println("\nTworzymy nowa liste studentow z ocenemi powyzej lub rowno 3.0 i wyswietlamy ja");
-//        StudentList studentsAboveOrEquals3 = studentList.getStudentsAboveOrEquals3();
-//        studentsAboveOrEquals3.printAllStudents();
 
     }
 
