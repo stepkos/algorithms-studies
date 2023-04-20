2442f2a419143ff59783c4eda1200d4e874ed63e
stepkos <stepkos@wp.pl>
2023-04-20 23:40:19 +0200

    Implements algorithms with statistic

--- 6. Sortowania szybkie/AlgorithmsComparison/src/main/java/AlgorithmsComparison.java
+++ 6. Sortowania szybkie/AlgorithmsComparison/src/main/java/AlgorithmsComparison.java
@@ -2,7 +2,14 @@ import java.util.Random;
 
 public class AlgorithmsComparison {
 
+    public static int mergeSortCompCounter = 0;
+    public static int mergeSortRewriteCounter = 0;
+    public static int quickSortCompCounter = 0;
+    public static int quickSortRewriteCounter = 0;
+
     public static int[] selectionSort(int[] array) {
+        System.out.println("\nSelection sort");
+
         int compCounter = 0;
         int rewriteCounter = 0;
 
@@ -28,6 +35,8 @@ public class AlgorithmsComparison {
     }
 
     public static int[] insertionSort(int[] array) {
+        System.out.println("\nInsertion sort");
+
         int compCounter = 0;
         int rewriteCounter = 0;
         int[] result = array.clone();
@@ -52,7 +61,13 @@ public class AlgorithmsComparison {
     }
 
     public static int[] mergeSort(int[] array) {
-        return mergeSort(array, 0, array.length - 1);
+        System.out.println("\nMerge sort");
+        mergeSortCompCounter = 0;
+        mergeSortRewriteCounter = 0;
+        int[] result = mergeSort(array.clone(), 0, array.length - 1);
+        System.out.println("Comp counter: " + mergeSortCompCounter);
+        System.out.println("Rewrite Counter: " + mergeSortRewriteCounter);
+        return result;
     }
 
     private static int[] mergeSort(int[] array, int startIdx, int endIdx) {
@@ -76,61 +91,73 @@ public class AlgorithmsComparison {
             if (left[leftIdx] <= right[rightIdx]) {
                 result[resultIdx] = left[leftIdx];
                 leftIdx++;
-            }
-            else {
+            } else {
                 result[resultIdx] = right[rightIdx];
                 rightIdx++;
             }
+            mergeSortCompCounter++;
+            mergeSortRewriteCounter++;
             resultIdx++;
         }
 
         while (leftIdx < left.length) {
             result[resultIdx] = left[leftIdx];
             leftIdx++;
+            mergeSortRewriteCounter++;
             resultIdx++;
         }
         while (rightIdx < right.length) {
             result[resultIdx] = right[rightIdx];
             rightIdx++;
+            mergeSortRewriteCounter++;
             resultIdx++;
         }
 
         return result;
     }
 
-    public static int[] quickSort(int[] originalArr) {
-        int[] arr = originalArr.clone();
-        quickSort(arr, 0, arr.length - 1);
-        return arr;
+    public static int[] quickSort(int[] arr) {
+        System.out.println("\nQuick sort");
+        quickSortCompCounter = 0;
+        quickSortRewriteCounter = 0;
+        int[] copyArr = arr.clone();
+        quickSort(copyArr, 0, copyArr.length - 1);
+        System.out.println("Comp counter: " + quickSortCompCounter);
+        System.out.println("Rewrite Counter: " + quickSortRewriteCounter);
+        return copyArr;
     }
 
     private static void quickSort(int[] arr, int startIdx, int endIdx) {
-        if (endIdx - startIdx <= 1) return;
-        int partition = partition(arr, startIdx, endIdx);
-        quickSort(arr, startIdx, partition);
-        quickSort(arr, partition + 1, endIdx);
+        if (startIdx >= endIdx) return;
+        int pivotIdx = partition(arr, startIdx, endIdx);
+        quickSort(arr, startIdx, pivotIdx - 1);
+        quickSort(arr, pivotIdx + 1, endIdx);
     }
 
     private static int partition(int[] arr, int startIdx, int endIdx) {
         int pivotIdx = startIdx + new Random().nextInt(endIdx - startIdx + 1);
         int pivot = arr[pivotIdx];
         swap(arr, pivotIdx, endIdx);
-        int left = startIdx;
-        int right = endIdx - 1;
-        while (left <= right) {
-            while (left <= right && arr[left] - pivot <= 0) left++;
-            while (left <= right && arr[right] - pivot > 0) right--;
-            if (left < right) swap(arr, left, right);
+        quickSortRewriteCounter += 3;
+        int i = startIdx;
+        for (int j = startIdx; j < endIdx; j++) {
+            if (arr[j] < pivot) {
+                swap(arr, i, j);
+                quickSortCompCounter++;
+                quickSortRewriteCounter += 3;
+                i++;
+            }
         }
-        swap(arr, left, endIdx);
-        return left;
+        swap(arr, i, endIdx);
+        quickSortRewriteCounter += 3;
+        return i;
     }
 
-    private static void swap(int[] arr, int left, int right) {
-        if (left == right) return;
-        int tmp = arr[left];
-        arr[left] = arr[right];
-        arr[right] = tmp;
+    private static void swap(int[] arr, int i, int j) {
+        if (i == j) return;
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 
     private static void demo1() {
@@ -144,11 +171,13 @@ public class AlgorithmsComparison {
             System.out.print(array[i] + " ");
         }
 
-        System.out.println("\n\nSelection sort");
         sortedArray = selectionSort(array);
 
-        System.out.println("\nInsertion sort");
         sortedArray = insertionSort(array);
+
+        sortedArray = mergeSort(array);
+
+        sortedArray = quickSort(array);
 
 //        System.out.println();
 //        for (int i = 0; i < sortedArray.length; i++)
