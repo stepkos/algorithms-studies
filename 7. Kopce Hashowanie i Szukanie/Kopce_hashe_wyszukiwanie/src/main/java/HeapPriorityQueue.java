69240f9081837a0731dd979a1e7f8e4bd7561510
stepkos <stepkos@wp.pl>
2023-04-27 14:21:25 +0200

    lab6 - zad1

--- 7. Kopce Hashowanie i Szukanie/Kopce_hashe_wyszukiwanie/src/main/java/HeapPriorityQueue.java
+++ 7. Kopce Hashowanie i Szukanie/Kopce_hashe_wyszukiwanie/src/main/java/HeapPriorityQueue.java
@@ -3,84 +3,92 @@ import java.util.Comparator;
 import java.util.List;
 
 public class HeapPriorityQueue<T> {
-    private final List<T> _list;
-    private final Comparator<T> _comparator;
+    private final List<T> list;
+    private final Comparator<T> comparator;
 
     public HeapPriorityQueue(Comparator<T> comparator) {
-        _comparator = comparator;
-        _list = new ArrayList<T>();
+        this.comparator = comparator;
+        list = new ArrayList<T>();
+    }
+
+    public HeapPriorityQueue(Comparator<T> comparator, List<T> list) {
+        this.comparator = comparator;
+        this.list = list;
+        for (int i = list.size() / 2; i >= 0; i--) {
+            sink(i);
+        }
     }
 
     public void enqueue(T value) {
-        _list.add(value);
-        swim(_list.size() - 1);
+        list.add(value);
+        swim(list.size() - 1);
     }
 
     public void clear() {
-        _list.clear();
+        list.clear();
     }
 
     public int size() {
-        return _list.size();
+        return list.size();
     }
 
     public boolean isEmpty() {
-        return _list.isEmpty();
+        return list.isEmpty();
     }
 
     public T first() {
-        return isEmpty() ? null : _list.get(0);
+        return isEmpty() ? null : list.get(0);
     }
 
     public T dequeue() {
         if (isEmpty()) return null;
-        T result = _list.get(0);
-        if (_list.size() > 1) {
-            _list.set(0, _list.get(_list.size() - 1));
+        T result = list.get(0);
+        if (list.size() > 1) {
+            list.set(0, list.get(list.size() - 1));
             sink(0);
         }
-        _list.remove(_list.size() - 1);
+        list.remove(list.size() - 1);
         return result;
     }
 
     public void changePriority(int index, T value) {
-        if (index < 0 || index >= _list.size()) {
+        if (index < 0 || index >= list.size()) {
             throw new IndexOutOfBoundsException();
         }
-        _list.set(index, value);
+        list.set(index, value);
         swim(index);
         sink(index);
     }
 
     public T remove(int index) {
-        if (index < 0 || index >= _list.size()) {
+        if (index < 0 || index >= list.size()) {
             throw new IndexOutOfBoundsException();
         }
-        T result = _list.get(index);
-        int last = _list.size() - 1;
+        T result = list.get(index);
+        int last = list.size() - 1;
         swap(index, last);
-        _list.remove(last);
+        list.remove(last);
         if (index == last) {
             return result;
         }
-        T newValue = _list.get(index);
+        T newValue = list.get(index);
         sink(index);
-        if (_list.get(index).equals(newValue)) {
+        if (list.get(index).equals(newValue)) {
             swim(index);
         }
         return result;
     }
 
     private void swap(int index1, int index2) {
-        T temp = _list.get(index1);
-        _list.set(index1, _list.get(index2));
-        _list.set(index2, temp);
+        T temp = list.get(index1);
+        list.set(index1, list.get(index2));
+        list.set(index2, temp);
     }
 
     // wynoszenie elementu w gorę - wersja iteracyjna
     private void swim(int index) {
         int parent;
-        while (index != 0 && _comparator.compare(_list.get(index), _list.get(parent = (index - 1) / 2)) > 0) {
+        while (index != 0 && comparator.compare(list.get(index), list.get(parent = (index - 1) / 2)) > 0) {
             swap(index, parent);
             index = parent;
         }
@@ -90,10 +98,10 @@ public class HeapPriorityQueue<T> {
     private void sink(int index) {
         boolean isDone = false;
         int child;
-        while (!isDone && (child = 2 * index + 1) < _list.size()) {
-            if (child < _list.size() - 1 && _comparator.compare(_list.get(child), _list.get(child + 1)) < 0)
+        while (!isDone && (child = 2 * index + 1) < list.size()) {
+            if (child < list.size() - 1 && comparator.compare(list.get(child), list.get(child + 1)) < 0)
                 ++child;
-            if (_comparator.compare(_list.get(index), _list.get(child)) < 0) {
+            if (comparator.compare(list.get(index), list.get(child)) < 0) {
                 swap(index, child);
                 index = child;
             } else
@@ -101,5 +109,11 @@ public class HeapPriorityQueue<T> {
         }
     }
 
-}
+    @Override
+    public String toString() {
+        return "HeapPriorityQueue{" +
+                "list=" + list.toString() +
+                '}';
+    }
 
+}
--- 7. Kopce Hashowanie i Szukanie/Kopce_hashe_wyszukiwanie/src/main/java/Main.java
+++ 7. Kopce Hashowanie i Szukanie/Kopce_hashe_wyszukiwanie/src/main/java/Main.java
@@ -1,7 +1,58 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class Main {
 
+    private static void demo1() {
+        List<Integer> list = new ArrayList<>(List.of(5, 7, 2, 1, 3, 8, 4));
+        HeapPriorityQueue<Integer> heap = new HeapPriorityQueue<>((a, b) -> a.compareTo(b), list);
+        heap.changePriority(3, 10);
+        heap.remove(2);
+        System.out.println("Kolejka: " + heap);
+
+        // Usuwanie elementów z kolejki i wyświetlanie ich
+        while (!heap.isEmpty()) {
+            System.out.println("Najwyższy priorytet: " + heap.dequeue());
+        }
+
+        // Sortowanie elementów tablicy przy użyciu kopca
+        Integer[] arr = {5, 1, 3, 4, 2};
+        heapSort(arr);
+
+        // Wyświetlanie posortowanej tablicy
+        System.out.print("Posortowana tablica: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    // Sortowanie tablicy przy użyciu kopca
+    private static <T extends Comparable<T>> void heapSort(T[] arr) {
+        HeapPriorityQueue<T> heap = new HeapPriorityQueue<>(Comparable::compareTo);
+
+        // Dodawanie elementów tablicy do kolejki
+        for (int i = 0; i < arr.length; i++)
+            heap.enqueue(arr[i]);
+
+        // Usuwanie elementów z kolejki i umieszczanie ich w tablicy w kolejności rosnącej
+        for (int i = 0; i < arr.length; i++)
+            arr[i] = heap.dequeue();
+    }
+
+    private static void demo2() {
+
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        // Zad 1
+        // zilustruj dzialanie
+//        demo1();
+
+        // Zad 2
+        demo2();
+
     }
 
 }
